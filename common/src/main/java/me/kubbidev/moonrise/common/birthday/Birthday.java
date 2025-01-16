3e2efe1e59e25b69aecc0fbe99ea6939b9e37205
kubbi <nicoladelaroche@gmail.com>
2025-01-16 01:42:35 +0100

    update: added birthday enumeration

---
+++ common/src/main/java/me/kubbidev/moonrise/common/birthday/Birthday.java
@@ -0,0 +1,26 @@
+//package me.kubbidev.moonrise.common.birthday;
+//
+//public enum Birthday {
+//    YAYOU(18, 2, 637522127452373003L),
+//    LUXUROW(28, 2, 469945103608446976L),
+//    SPARTIAT(5, 4, 536180603461173252L),
+//    RENA(9, 4, 904281685607211028L),
+//    KUBBI(20, 4, 537669034137616384L),
+//    TOMTOUMI(30, 7, 703286722884141076L),
+//    GABZ(14, 8, 509439886433189888L),
+//    NOMISSAKA(28, 8, 750332620935528448L),
+//    HDZL(8, 10, 854702147270344725L),
+//    NOLUXIS(29, 10, 876734806262677534L),
+//    GEDEON(6, 11, 630112009458810927L),
+//    LIEBE(12, 11, 769848994410659870L);
+//
+//    private final int day;
+//    private final int month;
+//    private final long userId;
+//
+//    Birthday(int day, int month, long userId) {
+//        this.day = day;
+//        this.month = month;
+//        this.userId = userId;
+//    }
+//}
